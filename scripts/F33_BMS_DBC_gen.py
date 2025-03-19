982dea3d79e14065f6f1139ce6d9f41203af2049
Sls <Solomonshulman13@gmail.com>
2025-03-19 18:52:18 -0400

    Added scripts dir

---
+++ scripts/F33_BMS_DBC_gen.py
@@ -0,0 +1,103 @@
+cells = 17
+segments = 8
+
+#Voltages
+f = open("voltages.txt", "w").close()
+f = open("voltages.txt", "w")
+
+length = 9
+seg = 0
+mul = 0
+first_start = 8
+start = first_start
+
+for i in range(cells * segments):
+    letter = chr(65 + seg) 
+    seg_num = letter + str(1 + (i % cells))
+   
+    if (start + length > 63):
+        start = first_start
+        mul += 1
+
+    if (i % cells == cells - 1):
+        seg += 1
+
+    f.write(' SG_ BMS_Voltages_{} m{} : {}|{}@1+ (0.01,0) [0|5.12] "V" Vector__XXX\n'.format(seg_num, mul, start, length))
+    start += length
+f.close()
+
+# Thermistor Voltages
+f = open("therm_voltages.txt", "w").close()
+f = open("therm_voltages.txt", "w")
+
+therms = 6
+length = 9
+seg = 0
+mul = 0
+first_start = 3
+start = first_start
+for i in range(therms * segments):
+    letter = chr(65 + seg)
+    seg_num = letter + str(1 + (i % therms ))
+
+    if (start + length > 63):
+        start = first_start
+        mul += 1
+
+    if (i % therms == therms - 1):
+        seg += 1
+
+    f.write(' SG_ BMS_ThermistorVoltages_{} m{} : {}|{}@1+ (0.01,0) [0|5.12] "V" Vector__XXX\n'.format(seg_num, mul, start, length))
+    start += length
+f.close()
+
+#Temps
+f = open("temps.txt", "w").close()
+f = open("temps.txt", "w")
+
+length = 8
+seg = 0
+mul = 0
+first_start = 3
+start = first_start
+
+for i in range(therms * segments):
+    letter = chr(65 + seg) 
+    seg_num = letter + str(1 + (i % therms))
+   
+    if (start + length > 63):
+        start = first_start
+        mul += 1
+
+    if (i % therms == therms - 1):
+        seg += 1
+
+    f.write(' SG_ BMS_Temperatures_{} m{} : {}|{}@1- (1,0) [-128|127] "C" Vector__XXX\n'.format(seg_num, mul, start, length))
+    start += length
+f.close()
+
+# Temps
+f = open("drain_status.txt", "w").close()
+f = open("drain_status.txt", "w")
+
+length = 1
+seg = 0
+mul = 0
+first_start = 2
+start = first_start
+
+for i in range(cells * segments):
+    letter = chr(65 + seg) 
+    seg_num = letter + str(1 + (i % cells))
+   
+    if (start + length > 64):
+        start = first_start
+        mul += 1
+
+    if (i % cells == cells - 1):
+        seg += 1
+
+    f.write(' SG_ BMS_DrainStatus_{} m{} : {}|{}@1+ (1,0) [0|1] "" Vector__XXX\n'.format(seg_num, mul, start, length))
+    start += length
+f.close()
+
---
+++ scripts/update.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+VIRTUALENV_DIR=virtualenv
+#if [ -z "$1" ];
+#then
+#    echo "First argument must be dbc filename"
+#    exit 1
+#fi
+
+#DBC_FILENAME=$(basename $1 .dbc)
+DBC_FILENAME=../main_dbc
+
+
+# Create virtualenv if doesn't exist
+if [ ! -d "$VIRTUALENV_DIR" ];
+then
+	echo "Creating virtualenv"
+	python3 -m venv $VIRTUALENV_DIR
+fi
+source $VIRTUALENV_DIR/bin/activate
+
+echo "Updating Requirements"
+pip3 install -U pip wheel cantools canmatrix > /dev/null
+
+echo "Building $DBC_FILENAME"
+cantools generate_c_source $DBC_FILENAME.dbc
+mv $DBC_FILENAME.c c_files/$DBC_FILENAME.c
+mv $DBC_FILENAME.h c_files/$DBC_FILENAME.h
+
+echo "Building sym"
+grep -v "SystemSignalLongSymbol" $DBC_FILENAME.dbc > $DBC_FILENAME.tmp.dbc
+canconvert $DBC_FILENAME.tmp.dbc $DBC_FILENAME.sym
+rm $DBC_FILENAME.tmp.dbc
