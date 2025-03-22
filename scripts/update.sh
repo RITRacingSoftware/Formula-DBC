b7a4973357af929d6e514523e54fb7c0d49db468
Sls <Solomonshulman13@gmail.com>
2025-03-21 22:44:14 -0400

    Fixed Drain Status mux in DBC

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,7 +8,7 @@ VIRTUALENV_DIR=virtualenv
 #fi
 
 #DBC_FILENAME=$(basename $1 .dbc)
-DBC_FILENAME=../main_dbc
+DBC_FILENAME=main_dbc
 
 
 # Create virtualenv if doesn't exist
@@ -24,8 +24,8 @@ pip3 install -U pip wheel cantools canmatrix > /dev/null
 
 echo "Building $DBC_FILENAME"
 cantools generate_c_source $DBC_FILENAME.dbc
-mv $DBC_FILENAME.c ../c_files/$DBC_FILENAME.c
-mv $DBC_FILENAME.h ../c_files/$DBC_FILENAME.h
+#mv ../$DBC_FILENAME.c ../c_files/$DBC_FILENAME.c
+#mv ../$DBC_FILENAME.h ../c_files/$DBC_FILENAME.h
 
 echo "Building sym"
 grep -v "SystemSignalLongSymbol" $DBC_FILENAME.dbc > $DBC_FILENAME.tmp.dbc
